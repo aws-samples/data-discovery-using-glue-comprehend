a0ca3891908159464eb39e921792444c21737cdb
Ahmed Raafat <78147641+arraafat@users.noreply.github.com>
2021-06-19 22:21:11 +0100

    Update comprehend_create_custom_entity.py

--- scripts/comprehend_create_custom_entity.py
+++ scripts/comprehend_create_custom_entity.py
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
     response = comprehend.create_entity_recognizer(
     RecognizerName="Recognizer-blog-v3".format(str(uuid.uuid4())),
     LanguageCode="en",
-    DataAccessRoleArn_user  = "arn:aws:iam::"+account_id+":role/Lambda-S3-Glue-comprehend",  # replace with your IAM user created before
+    DataAccessRoleArn  = "arn:aws:iam::"+account_id+":role/Lambda-S3-Glue-comprehend",  # replace with your IAM user created before
     InputDataConfig={
         'DataFormat': 'COMPREHEND_CSV',
         "EntityTypes": [
