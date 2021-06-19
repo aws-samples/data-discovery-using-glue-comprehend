bb8992731f4b60a7b4d95c054b46b605eb5b1295
Ahmed Raafat <78147641+arraafat@users.noreply.github.com>
2021-06-19 12:02:47 +0100

    Update glue_comprehend_workflow.py

--- scripts/glue_comprehend_workflow.py
+++ scripts/glue_comprehend_workflow.py
@@ -9,11 +9,11 @@ import boto3
 client = boto3.client('glue')
 
 # Variables for the job:
-glueJobName = "Glue_Comprehend_Job"
+glueJobName = "Glue_Comprehend_Job" # replace with your glue job name
 
 # Define Lambda function
 def lambda_handler(event, context):
 
-    response = client.start_job_run(JobName = <"Upadate with your glue job name">)
+    response = client.start_job_run(JobName = glueJobName)
 
     return response
